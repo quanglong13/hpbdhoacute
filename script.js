2bf0f0f864d55d25cff4fc4bebbfbc06f8100a4e
quanglong13 <89090221+quanglong13@users.noreply.github.com>
2023-02-27 14:28:26 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("UserName");
+ var b=prompt("Password");
+  	if(a== "Duy" && b=="140402")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
